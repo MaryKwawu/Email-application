d0c75ca5f71bf19640d4a6df570130333df2e699
MaryKwawu <may419316@gmail.com>
2024-09-10 04:01:25 +0000

    Chore : feat/Generate a random password change password, Set the alternate email, Set the mailbox capacity

--- src/Email.java
+++ src/Email.java
@@ -5,7 +5,7 @@ class Email{
     String lastname;
     String password;
     String department;
-    int mailboxCapacity;
+    int mailboxCapacity = 500;
     String alternateEmail;
      int defaultPasswordLength = 10;
      String email;
@@ -17,29 +17,29 @@ class Email{
     public Email(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastname = lastName;
-        System.out.println("Email CREATED: " + this.firstName + " " + this.lastname);
+//        System.out.println("Email CREATED: " + this.firstName + " " + this.lastname);
 
 
         //call a method asking for the department - return the department
         this.department = setDepartment();
-        System.out.println("Department: " + this.department);
+//        System.out.println("Department: " + this.department);
 
 
         //call a method that returns a random password
         this.password = randomPassword(defaultPasswordLength);
-        System.out.println("Your password is: " + this.password);
+//        System.out.println("Your password is: " + this.password);
 
 
         //combine element to generate email
         email = firstName.toLowerCase() + " , " + lastName.toLowerCase()  + "@" + department + "." + companySuffix;
-        System.out.println("Your email is: " + email);
+//        System.out.println("Your email is: " + email);
 
 
     }
 
     //Ask for the department
     private String setDepartment(){
-        System.out.print("DEPARTMENT CODES\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter department code:");
+        System.out.print("New worker: " + firstName + " . Enter the DEPARTMENT CODES\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter department code:");
         Scanner in = new Scanner(System.in);
         int depChoice = in.nextInt();
         if (depChoice == 1){
@@ -67,7 +67,36 @@ class Email{
     }
 
     //Set the mailbox capacity
+    public void setMailboxCapacity(int capacity){
+        this.mailboxCapacity = capacity;
+    }
 
     //Set the alternate email
+    public void setAlternateEmail(String altEmail){
+        this.alternateEmail = altEmail;
+    }
+
+
+    //change password
+    public void changePassword(String password){
+        this.password = password;
+
+    }
+
+    public int getMailboxCapacity(){
+        return mailboxCapacity;
+    }
+
+    public String getAlternateEmail(){
+        return alternateEmail;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public String showInfo(){
+        return "\nDisplay Name: " + firstName + " " + lastname + "" + "\nCompany Email: " + email + "\nMail Box Capacity: " + mailboxCapacity + "mb";
+    }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Email eml = new Email("Mary", "Kwawu");
 
+        System.out.println(eml.showInfo());
+
 
     }
 }
